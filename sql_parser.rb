4277e641f6ab54f14cc06fcbe581d99ac316b02f
Matt Roberts <roberts.mattroberts@gmail.com>
2011-12-20 20:24:02 +0000

    regex tweak

--- sql_parser.rb
+++ sql_parser.rb
@@ -5,22 +5,22 @@ class SqlParser
  end
 
  def parse
-  matches = /exec\s+sp_executesql\s+N'([\s\S]*)',\s*N'(@[\s|\S]*?)',([\s|\S]*)/.match(@sql_to_parse)
+  matches = /(exec)*\s*sp_executesql\s+N'([\s\S]*)',\s*N'(@[\s|\S]*?)',\s*([\s|\S]*)[;\z]/.match(@sql_to_parse)
 
   return "" unless matches != nil
   
-  params = matches[2].split(',')
+  params = matches[3].split(',')
   new_sql = "BEGIN\n"
 
-  params.each {|p| new_sql += "\nDECLARE #{p}"}
+  params.each {|p| new_sql += "\nDECLARE #{p.strip};"}
 
-  param_values = matches[3].split(',')
-  new_sql += "\n\n"
-  param_values.each {|p| new_sql += "\nSET #{p}"}
+  param_values = matches[4].split(',')
+  new_sql += "\n"
+  param_values.each {|p| new_sql += "\nSET #{p.strip};"}
 
   # Need to go here to format it :) http://sqlformat.appspot.com/
   result = Net::HTTP.post_form(URI.parse("http://sqlformat.appspot.com/format/"), 
-  	{"data" => matches[1].to_s.gsub(/''/,"'"), "format" => "text", "keyword_case" => "upper", "reindent" => "true", "n_indents" => "2"})
+  	{"data" => matches[2].to_s.gsub(/''/,"'"), "format" => "text", "keyword_case" => "upper", "reindent" => "true", "n_indents" => "2"})
 	
   new_sql += "\n" + result.body
    
